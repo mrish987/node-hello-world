e8ddcc9530ff453e93721f43952976a1ede52297
mrish <mrish@my.bridgeport.edu>
2024-03-30 17:01:17 -0400

    Update Docker Hub repository in CircleCI config

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const host = '0.0.0.0';
 
 app.get('/', (req, res) => {
   res.send('How is your day going? Congratulations on your Node Hello World Application built on Docker!');
-})
+});
 
 app.listen(port, host);
 console.log(`Running on http://${host}:${port}`);
